63baf14afd3c73d861f3456daac5153871d97983
Fernando <ferg.developer@gmail.com>
2023-06-06 16:07:57 -0400

    iterating complex objs

--- src/app/products/product.component.ts
+++ src/app/products/product.component.ts
@@ -4,14 +4,41 @@ declare const getRandomNumbers:any;
 
 @Component({
     selector: 'app-product',
-    template: `<h2>Product List</h2> 
-                <div *ngIf='showDiv'>Product 1</div>
-                <div>{{rNum}}</div>
+    template: `
+        <div>Random Numbers: {{rNum}}</div>
+
+        <div>All Departments</div>
+        <li *ngFor="let dept of departments">
+            {{dept}}
+        </li>
+        <div>All Employees</div>
+        <li *ngFor="let empl of employees">
+            {{empl.name}},
+            {{empl.department}}
+        </li>
     `,
-    styles: ['div {font-weight:bolder; color:darkgreen;}']
+    styles: ['div {font-weight:bolder; color:darkgreen;}', 'li  {font-weight:bolder; color:red;}']
 })
 
 export class ProductComponent{
     showDiv=true;
     rNum = <[]>getRandomNumbers();
+    departments = ['Mountain Bicycles', 'Road Bikes', 'Hybrid Bikes', 'Bicycle Pumps']
+    employees = [
+        {
+            name: 'Fernando',
+            employeeId: 1234,
+            department: "IT"
+        },
+        {
+            name: 'Thomas',
+            employeeId: 4567,
+            department: "Sales"
+        },
+        {
+            name: 'XYZ',
+            employeeId: 8910,
+            department: "IT"
+        }
+    ]
 };
